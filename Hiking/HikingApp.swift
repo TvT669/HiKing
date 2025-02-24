6aec84f5cd55652ad4ac79e12c6c591ed0359d5d
TvT669 <3172736837@qq.com>
2025-02-24 20:42:56 +0800

    Initial Commit

---
+++ Hiking/HikingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikingApp.swift
+//  Hiking
+//
+//  Created by 珠穆朗玛小蜜蜂 on 2025/2/24.
+//
+
+import SwiftUI
+
+@main
+struct HikingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
